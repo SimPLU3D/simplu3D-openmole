54a6c9df61280a7ea84e258306297072967de7b6
julienperret <julien.perret@gmail.com>
2015-10-22 11:31:03 +0200

    creating the directory outside of the plugin

--- src/main/scala/simplu3dopenmoleplugin/Simplu3DTask.scala
+++ src/main/scala/simplu3dopenmoleplugin/Simplu3DTask.scala
@@ -4,13 +4,11 @@ import java.io.File
 import fr.ign.cogit.simplu3d.openmole._
 
 object Simplu3DTask {
-  def apply(inputFolder: File, paramFile: File,
+  def apply(inputFolder: File, outputFolder: File, paramFile: File,
     idBPU: Int, distReculVoirie: Double, distReculFond: Double,
     distReculLat: Double, maximalCES: Double, hIniRoad: Double,
     slopeRoad: Double, hauteurMax: Double, seed: Long): (Double, Double, Long, File) = {
-    val outputFoler = new File("/tmp/data_out")
-    outputFoler.mkdir()
-    val res = RunTask.run(inputFolder, outputFoler, paramFile, idBPU, distReculVoirie, distReculFond, distReculLat, maximalCES, hIniRoad, slopeRoad, hauteurMax, seed)
-    (res.energy, res.coverageRatio, res.signature, outputFoler)
+    val res = RunTask.run(inputFolder, outputFolder, paramFile, idBPU, distReculVoirie, distReculFond, distReculLat, maximalCES, hIniRoad, slopeRoad, hauteurMax, seed)
+    (res.energy, res.coverageRatio, res.signature, outputFolder)
   }
 }
