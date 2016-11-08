1e1bd88a18923909de06440ddebfd209f2f94af8
chapinux <paul.chapron@gmail.com>
2016-11-08 17:54:38 +0100

    new scalatask definition for simulating at the whole zone scale

---
+++ src/main/scala/simplu3dopenmoleplugin/Simplu3DTask2.scala
@@ -0,0 +1,24 @@
+package simplu3dopenmoleplugin
+
+import java.io.File
+import fr.ign.cogit.simplu3d.experiments.openmole._
+
+//object Simplu3DTask {
+// def apply(inputFolder: File, outputFolder: File, paramFile: File,
+//  idBPU: Int, distReculVoirie: Double, distReculFond: Double,
+// distReculLat: Double, maximalCES: Double, hIniRoad: Double,
+// slopeRoad: Double, hauteurMax: Double, seed: Long): (Double, Double, Long, File) = {
+//   val res = RunTask.run(inputFolder, outputFolder, paramFile, idBPU, distReculVoirie, distReculFond, distReculLat, maximalCES, hIniRoad, slopeRoad, hauteurMax, seed)
+//  (res.energy, res.coverageRatio, res.signature, outputFolder)
+//  }
+//}
+
+object Simplu3DTask2 {
+  def apply(inputFolder: File, outputFolder: File, paramFile: File,
+    distReculVoirie: Double, distReculFond: Double,
+    distReculLat: Double, maximalCES: Double, hIniRoad: Double,
+    slopeRoad: Double, hauteurMax: Double, seed: Long): (Double, Double, Long, File) = {
+    val res = RunTask.run2(inputFolder, outputFolder, paramFile, distReculVoirie, distReculFond, distReculLat, maximalCES, hIniRoad, slopeRoad, hauteurMax, seed)
+    (res.energy, res.coverageRatio, res.signature, outputFolder)
+  }
+}
